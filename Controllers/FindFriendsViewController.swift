c68f9f771625fc31a89d9fa92a8aceaa3aca62c8
Emily Jewik <emilyjewik@gmail.com>
2018-07-10 16:41:29 -0700

    commit will finish pg 11

---
+++ Controllers/FindFriendsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Emily Jewik on 7/10/18.
+//  Copyright © 2018 Emily Jewik. All rights reserved.
+//
+
+import Foundation
+
+class FindFriendsViewController {
+    
+}
