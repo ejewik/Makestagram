d20e45440a3745d237fa26b65cd8d842ee9c807b
Emily Jewik <emilyjewik@gmail.com>
2018-07-11 09:47:19 -0700

    Commit will upload creating a helper pg 13

--- Helpers/MGPhotoHelper.swift
+++ Helpers/MGPhotoHelper.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class MGPhotoHelper: NSObject {
-
+    //Mark: Properties
+    
+    var completionHandler: ((UIImage) -> Void)?
+    
+    //Mark: - Helper Methods
+    
+    func presentActionSheet(from viewController: UIViewController) {
+        let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?" , preferredStyle: .actionSheet )
+        
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { action in
+                
+            })
+            alertController.addAction(capturePhotoAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { action in
+                
+            })
+            alertController.addAction(uploadAction)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        viewController.present(alertController, animated: true)
+    }
 }
