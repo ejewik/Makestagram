ff991386c82e7902e204ed2bbd88bcca7e2f4ba3
Emily Jewik <emilyjewik@gmail.com>
2018-07-11 16:49:48 -0700

    Commit will finish creating a post action cell pg 18

---
+++ Views/PostActionCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostActionCell.swift
+//  Makestagram
+//
+//  Created by Emily Jewik on 7/11/18.
+//  Copyright © 2018 Emily Jewik. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PostActionCell : UITableViewCell {
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var timeAgoLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    @IBAction func likeButtonTapped(_ sender: UIButton) {
+        print("like button tapped")
+    }
+    
+}
